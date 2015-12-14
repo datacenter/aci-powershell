72c609387d07c55fdbc522b1de4e7318d674b8c5
Chris Paggen <cpaggen@gmail.com>
2015-12-14 22:45:49 +0100

    Create simpleRest.ps1

---
+++ simpleRest.ps1
@@ -0,0 +1,26 @@
+# super simple powershell for ACI
+# first, we log in, we extract the cookie
+# and use it for subsequent requests
+
+param (
+  [string]$username,
+  [string]$password,
+  [string]$apic
+)
+
+$creds = '<aaaUser name="' + $username + '" pwd="' + $password + '"/>'
+$baseurl = "http://" + $apic
+$url = $baseurl + "/api/aaaLogin.xml"
+$r = Invoke-RestMethod -Uri $url -Method Post -SessionVariable s -Body $creds
+$cookies = $s.Cookies.GetCookies($url)
+
+$tenanturl = $baseurl + "/api/node/class/fvTenant.json?"
+$web = new-object net.webclient
+$web.Headers.add("Cookie", $cookies)
+$result = $web.DownloadString($tenanturl)
+$resultjson = $result | ConvertFrom-Json
+foreach( $tenant in $resultjson.imdata ) 
+{
+    $tn = "Found tenant " + $tenant.fvTenant.attributes.name
+    write $tn
+}
